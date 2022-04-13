31e37d25ed0557ad83e6becca4bf23e2b26164c6
TygFF11 <kh091216@icloud.com>
2022-04-13 10:31:30 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :rooms, only:[:index,:new,:create]
+  resources :rooms, only:[:new,:create] do
+    resources :messages, only:[:index,:create]
+  end
+
   devise_for :users
   root to: "rooms#index"
   resources :users, only:[:edit,:update]
